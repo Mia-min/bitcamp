8b38635a70088ad085a322592ddb8c190134d7d9
Mia-min <woal274@naver.com>
2017-09-22 11:55:03 +0900

    '.'

---
+++ java-basic/Test03.java
@@ -0,0 +1,7 @@
+/*.class 파일 분리하기
+ */
+class Test03 { 
+  public static void main(String[] args) { 
+    System.out.println("안녕하세요!"); 
+  } 
+} 
---
+++ node-basic/hello.js
@@ -0,0 +1,3 @@
+//hello.js
+//인터프리터 방식
+console.log("Hello, world!")
